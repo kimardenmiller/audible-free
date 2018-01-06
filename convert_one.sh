421875c7bab73cc2338e26dfd5433b79a8349053
kimardenmiller <kimardenmiller@gmail.com>
2018-01-06 12:10:35 -0800

    AUDIBLE_ACTIVATION environment variable method created

--- convert_one.sh
+++ convert_one.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#ACTIVATION_BYTES=${} # Seems to need to be manually entered as you can see this value below.
+#Requires AUDIBLE_ACTIVATION environment variable (best set using EnvPane @ https://github.com/hschmidt/EnvPane)
 SOURCE_DIR='/Users/kimardenmiller/Downloads'
-SOURCE_FILE='TheSoundandtheFuryUnabridged'
+SOURCE_FILE='PowerfullyConfidentInternetDatingBetheGuyThatWomenWanttoMeetOnlineUnabridged'
 SOURCE_PATH=${SOURCE_DIR}/${SOURCE_FILE}.aax
 OUTPUT_DIR='/Volumes/Document External Archive/Audible Library'
 OUTPUT_PATH=${OUTPUT_DIR}/${SOURCE_FILE}
 
-ffmpeg -activation_bytes activation_bytes_here -i "${SOURCE_PATH}" -vn -c:a copy "${OUTPUT_PATH}".m4a;mv "${OUTPUT_PATH}".m4{a,b}
+ffmpeg -activation_bytes ${AUDIBLE_ACTIVATION} -i "${SOURCE_PATH}" -vn -c:a copy "${OUTPUT_PATH}".m4a;mv "${OUTPUT_PATH}".m4{a,b}
